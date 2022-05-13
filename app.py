a95900b276637f9b796e20e5366a56e4610b937e
Soegidev <soegidev.id@gmail.com>
2022-05-13 09:33:46 +0700

    INITial

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 """A dummy docstring."""
-from flask import Flask,jsonify
 import os
+from flask import Flask,jsonify
 app = Flask(__name__)
 HOST = "0.0.0.0"
 PORT = 5000
 @app.route("/")
 def hello_world():
+    """A multi-line
+    docstring.
+    """
     env = os.environ.get('FLASK_ENV')
-    """A dummy docstring."""
     return jsonify({
         "data": f"Hello World {env}"
     })
